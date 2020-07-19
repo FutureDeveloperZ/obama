e8b300e5f7856a07dd302e02deb62d833ad57fe3
Wolfkid200444 <wolfkid200444@gmail.com>
2020-07-19 16:20:46 -0400

    Quotes

--- src/Events/ready.js
+++ src/Events/ready.js
@@ -10,9 +10,9 @@ module.exports = class extends Event {
   
   run() {
     console.log([
-      `Logged in as ${this.client.user.tag}`
-      `Loaded ${this.client.commands.size} commands`
-      `Loaded ${this.client.events.size} events`
+      "Logged in as ${this.client.user.tag}",
+      "Loaded ${this.client.commands.size} commands",
+      "Loaded ${this.client.events.size} events"
       ].join('\n'));
   }
   

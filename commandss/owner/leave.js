f46eb5668f579de5462768c390340ee98f2a0ddc
Wolfkid200444 <wolfkid200444@gmail.com>
2020-07-20 10:30:07 -0400

    leave command

--- commandss/owner/leave.js
+++ commandss/owner/leave.js
@@ -1,21 +1,36 @@
 const Discord = require('discord.js');
-const config = require('../../config.json');
+const Command = require('../../Structure/Command');
+const config = require('../../../config.json');
 
-module.exports.run = async (client, message, args) => {
-  if (message.author.id !== `${config.ownerid}`) return errors.ownersOnly(message);
-  if (message.author.id == `${config.ownerid}`) {
+module.exports = class extends Command {
+
+
+constructor(...args) {
+		super(...args, {
+			aliases: ['bye'],
+			description: 'he leaves the server',
+			category: 'Owner',
+			usage: 'leave'
+		});
+	}
+
+  async run(message) {
+  
+
+  
+let embed = new Discord.MessageEmbed()
+  .setTitle("leave")
+  .setDescription("Sorry, the `leave` command can only be executed by the Main Developer.")
+  .setColor("#cdf785");
+  if(!this.client.owners.includes(message.author.id)) return message.channel.send(embed);
+  if (this.client.owners.includes(message.author.id)) {
     let embed = new Discord.MessageEmbed()
-      .setTitle(`It's time for ${client.user.username} to go!!!`)
+      .setTitle(`It's time for ${this.client.user.username} to go!!!`)
       .setColor(config.red)
       .setDescription('Seeee ya Suckers!...')
     message.channel.send(embed);
     message.guild.leave();
     return
-  }
-
-};
-
-module.exports.help = {
-  name: 'leave'
-
+         }
+    }
 };

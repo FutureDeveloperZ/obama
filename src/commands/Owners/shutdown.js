7939e8ddea9edcf97cff345c114cefbffb902caa
Wolfkid200444 <wolfkid200444@gmail.com>
2020-07-20 14:15:19 -0400

    Added Leave

---
+++ src/commands/Owners/shutdown.js
@@ -0,0 +1,34 @@
+const Discord = require('discord.js');
+const Command = require('../../Structure/Command');
+const config = require('../../config.json');
+
+module.exports = class extends Command {
+
+constructor(...args) {
+		super(...args, {
+			aliases: ['off'],
+			description: 'shutdowns the bot',
+			category: 'Owner',
+			usage: ''
+		});
+	}
+
+ async run(message) {
+  
+  
+let embed = new Discord.MessageEmbed()
+  .setTitle("Shutdown")
+  .setDescription("Sorry, the `shutdown` command can only be executed by the Main Developer.")
+  .setColor("#cdf785");
+  if(!this.client.owners.includes(message.author.id)) return message.channel.send(embed);
+ 
+  if (this.client.owners.includes(message.author.id)) {
+    let embed = new Discord.MessageEmbed()
+      .setTitle('Shutting Down...')
+      .setColor(config.red);
+
+    message.channel.send(embed)
+      .then(message => client.destroy())
+      }
+  }
+}

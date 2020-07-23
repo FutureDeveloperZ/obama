fb639234b0fe0c4b5da71f4555ad99b3ab3fe9a5
Wolfkid200444 <wolfkid200444@gmail.com>
2020-07-23 08:26:51 -0400

    Magick

--- src/commands/Memes/ifunny.js
+++ src/commands/Memes/ifunny.js
@@ -14,7 +14,7 @@ module.exports = class extends Command {
 	}
 
 	async run(message) {
-  channel.startTyping();
+  message.channel.startTyping();
   const image = await require("../utils/imagedetect.js")(message);
   if (image === undefined) return `${message.author.mention}, you need to provide an image to add a iFunny watermark!`;
   const buffer = await promisify(magick.watermark)(image.path, "./assets/images/ifunny.png", 8, true, true, false, image.type.toUpperCase(), image.delay ? (100 / image.delay.split("/")[0]) * image.delay.split("/")[1] : 0);
@@ -22,6 +22,6 @@ module.exports = class extends Command {
     file: buffer,
     name: `ifunny.${image.type}`
   };
-  channel.stopTyping(true);
+  message.channel.stopTyping(true);
 };
 }

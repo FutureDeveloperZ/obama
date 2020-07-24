0ac35b2bcb5f8fa2e7277f1e1802402fcfc7d974
Wolfkid200444 <wolfkid200444@gmail.com>
2020-07-24 09:00:45 -0400

    Another Fix

--- src/commands/Search/roblox.js
+++ src/commands/Search/roblox.js
@@ -7,7 +7,7 @@ module.exports = class extends Command {
   
 constructor(...args) {
 		super(...args, {
-			aliases: [''],
+			aliases: [],
 			description: 'Search up your roblox profile',
 			category: 'Search',
 			usage: 'Roblox <username>'

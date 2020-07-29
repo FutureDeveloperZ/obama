b3ca54952279822550b0e6b55c11186cef3b0bb2
Wolfkid200444 <wolfkid200444@gmail.com>
2020-07-29 18:51:29 -0400

    Twig

---
+++ src/commands/fun/twig.js
@@ -0,0 +1,29 @@
+const { MessageEmbed} = require("discord.js");
+const Command = require('../../Structure/Command');
+
+
+module.exports = class extends Command {
+  
+  
+constructor(...args) {
+		super(...args, {
+			aliases: [],
+			description: 'SECRET',
+			category: 'Fun',
+			usage: 'SECRET'
+		});
+	}
+
+
+async run(message, args) {
+  
+  if (message.author.id !== '650536741068865537') {
+    return message.channel.send('YOU ARE NOT TWIG DUMB ASS');
+  }
+  
+  let Embed = new MessageEmbed()
+  .setAuthor('TWIG')
+  .addField('YES')
+  message.channel.send(Embed);
+}
+}

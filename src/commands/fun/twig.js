01dd3c7b3e9aab152f13b4252e43b9527894ff1f
Wolfkid200444 <wolfkid200444@gmail.com>
2020-07-29 19:28:52 -0400

    Twig smexy
    git

--- src/commands/fun/twig.js
+++ src/commands/fun/twig.js
@@ -20,10 +20,18 @@ async run(message, args) {
   if (message.author.id !== '650536741068865537') {
     return message.channel.send('YOU ARE NOT TWIG DUMB ASS');
   }
-  
+  var quotes = [
+    "cockaine",
+    "Yesyes",
+    "Hot",
+    "Yes",
+    "HAHAHHAHAHAHAHAHAHAHAHAHAHAHA",
+    "Woah dope",
+    "Okie",
+    ]
   let Embed = new MessageEmbed()
-  .setAuthor('TWIG')
-  .setDescription('YES')
+  .setAuthor('TWIG QUOTES')
+  .setDescription(quotes[Math.floor(Math.random() * quotes.length)])
   message.channel.send(Embed);
 }
 }

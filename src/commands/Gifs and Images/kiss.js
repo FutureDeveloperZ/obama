b20aa74fe6d3d118bacce8f4fccee9e6179c66d9
Wolfkid200444 <wolfkid200444@gmail.com>
2020-07-30 11:45:37 -0400

    Smoshy

--- src/commands/Gifs and Images/kiss.js
+++ src/commands/Gifs and Images/kiss.js
@@ -1,14 +1,28 @@
 const { MessageEmbed } = require("discord.js");
 const superagent = require('superagent');
+const Command = require('../../Structure/Command');
 
 
-module.exports.run = async (bot, message, args) => {
+module.exports = class extends Command {
+  
+  
+constructor(...args) {
+		super(...args, {
+			aliases: [],
+			description: 'Kiss somebody',
+			category: 'Gif and Images',
+			usage: 'kiss <user>'
+		});
+	}
+
+
+async run(message, args) {
     let user = message.mentions.users.first() || message.author;
    superagent.get("https://nekos.life/api/kiss", (err, res) => {
         if (err) { return console.log(`An error Occured while running kiss command. Ran by ${message.author.tag}.\nError: ${err}`)}
     else{
         message.channel.send("", { embed: new MessageEmbed()
-                                  .setTitle(`${message.author.username} kissed ${user.username}`)
+                                  .setDescription(`**${message.author.username}** kissed **${user.username}**`)
                                   .setColor(`${message.guild.me.displayHexColor!=='#FFFFFF' ? message.guild.me.displayHexColor : 0xffffff}`)
                                   .setImage(res.body.url)
                                   //.setFooter(`Requested by ${message.author.tag}`)
@@ -17,6 +31,5 @@ module.exports.run = async (bot, message, args) => {
     })
 }
 
-module.exports.help = {
-  name:"kiss"
+
 }
--- src/commands/Gifs and Images/pat.js
+++ src/commands/Gifs and Images/pat.js
@@ -1,14 +1,28 @@
 const { MessageEmbed } = require("discord.js");
 const superagent = require('superagent');
+const Command = require('../../Structure/Command');
 
 
-module.exports.run = async (bot, message, args) => {
+module.exports = class extends Command {
+  
+  
+constructor(...args) {
+		super(...args, {
+			aliases: [],
+			description: 'Pat somebody',
+			category: 'Gif and Images',
+			usage: 'pat <user>'
+		});
+	}
+
+
+async run(message, args) {
     let user = message.mentions.users.first() || message.author;
    superagent.get("https://nekos.life/api/pat", (err, res) => {
         if (err) { return console.log(`An error Occured while running pat command. Ran by ${message.author.tag}.\nError: ${err}`)}
     else{
         message.channel.send("", { embed: new MessageEmbed()
-                                  .setTitle(`${message.author.username} pat ${user.username}`)
+                                  .setDescription(`**${message.author.username}** pat **${user.username}**`)
                                   .setColor(`${message.guild.me.displayHexColor!=='#FFFFFF' ? message.guild.me.displayHexColor : 0xffffff}`)
                                   .setImage(res.body.url)
                                   //.setFooter(`Requested by ${message.author.tag}`)
@@ -17,6 +31,5 @@ module.exports.run = async (bot, message, args) => {
     })
 }
 
-module.exports.help = {
-  name:"pat"
+
 }

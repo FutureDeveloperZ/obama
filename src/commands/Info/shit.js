5270dc6f02d6d0b5fc8bd03934654336c57d4307
Wolfkid200444 <wolfkid200444@gmail.com>
2020-07-31 17:37:52 -0400

    Help

--- src/commands/Info/shit.js
+++ src/commands/Info/shit.js
@@ -1,4 +1,4 @@
-const Command = require('../../structures/Command');
+const Command = require('../../Structures/Command');
 const { MessageEmbed } = require('discord.js');
 
 module.exports = class Help extends Command {

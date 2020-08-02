f791633023162a2404ba6abf4a2c3b1c6a16a18d
Wolfkid200444 <wolfkid200444@gmail.com>
2020-08-02 11:35:19 -0400

    Another one

---
+++ src/commands/Memes/yousuck.js
@@ -0,0 +1,60 @@
+const request = require('node-superfetch');
+const fsn = require("fs-nextra");
+const Command = require('../../Structure/Command');
+
+module.exports = class extends Command {
+
+	constructor(...args) {
+		super(...args, {
+			aliases: [],
+			description: '3000 avatar',
+			category: 'Memes',
+			usage: ''
+		});
+	}
+	
+async run(message, args) {
+  const {
+    Canvas
+  } = require('canvas-constructor');
+  if (message.mentions.users.size < 1) return send("You didn't mention a user!");
+  let a = message.mentions.users.first();
+  const getSlapped = async (person) => {
+    const plate = await fsn.readFile('./src/assets/images/suck.png');
+    const png = person.replace('.gif', '.png');
+    const {
+      body
+    } = await request.get(png);
+    return new Canvas(979, 531)
+      .addImage(plate, 0, 0, 979, 531)
+      .setTextFont('bold 50px Comic Sans MS')
+      .setTextAlign('center')
+      .addText(`Dear ${a.username},`, 500, 225)
+      .addText(`You SUCK!`, 500, 325)
+      .toBuffer();
+  };
+  try {
+    if (message.mentions.users.size < 1) {
+      const person = message.author.didplayAvatarURL({ format: 'jpg' });
+      const result = await getSlapped(person);
+      await message.channel.send({
+        files: [{
+          attachment: result,
+          name: 'usuck.png'
+        }]
+      });
+    } else {
+      const person = message.mentions.users.first().displayAvatarURL({ format: 'jpg' });
+      const result = await getSlapped(person);
+      await message.channel.send({
+        files: [{
+          attachment: result,
+          name: 'usuck.png'
+        }]
+      });
+    }
+  } catch (error) {
+    throw error;
+  }
+};
+}

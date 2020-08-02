8f042b85923e204ad9c6f20208fe74b7a40fc960
Wolfkid200444 <wolfkid200444@gmail.com>
2020-08-02 17:03:47 -0400

    Fix rps

--- src/commands/fun/rps.js
+++ src/commands/fun/rps.js
@@ -18,9 +18,9 @@ async run(message, args) {
     if (!validPicks.includes(userPick) || !userPick) return message.channel.send(`Your pick wasn't rock, paper, nor scrissors (not a valid pick)!`, true);
     const botPicked = validPicks[Math.floor(Math.random() * validPicks.length)];
     //Rock
-    if (userPick == "rock" && botPicked == "rock") return messagee.channel.send(`You picked rock and I also picked rock. It's a tie!`);
-    if (userPick == "rock" && botPicked == "paper") return message.channel.send(`You picked rock and I also picked rock. You beat me! :rage:`);
-    if (userPick == "rock" && botPicked == "scrissors") return message.channel.send(`You picked rock and I also picked rock. I beat you! :sweat_smile:`);
+    if (userPick == "rock" && botPicked == "rock") return messagee.channel.send(`You picked ${userPick} and I picked ${botPicked}. It's a tie!`);
+    if (userPick == "rock" && botPicked == "paper") return message.channel.send(`You picked ${userPick} and I picked ${botPicked}. You beat me! :rage:`);
+    if (userPick == "rock" && botPicked == "scrissors") return message.channel.send(`You picked ${userPick} and I picked ${botPicked}. I beat you! :sweat_smile:`);
     //Paper
     if (userPick == "paper" && botPicked == "paper") return message.channel.send(`You picked ${userPick} and I picked ${botPicked}. It's a tie!`);
     if (userPick == "paper" && botPicked == "rock") return message.channel.send(`You picked ${userPick} and I picked ${botPicked}. You beat me! :rage:`);

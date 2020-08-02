ff85108ae68ad8416b48a1c8d6b4d20abb408608
Wolfkid200444 <wolfkid200444@gmail.com>
2020-08-02 17:26:38 -0400

    Grammar Mistake

--- src/commands/fun/rps.js
+++ src/commands/fun/rps.js
@@ -13,22 +13,22 @@ module.exports = class extends Command {
 	}
 	
 async run(message, args) {
-    const validPicks = ["rock", "paper", "scrissors"];
+    const validPicks = ["rock", "paper", "scissors"];
     const userPick = args.join(` `).toLowerCase();
     if (!validPicks.includes(userPick) || !userPick) return message.channel.send(`Your pick wasn't rock, paper, nor scrissors (not a valid pick)!`, true);
     const botPicked = validPicks[Math.floor(Math.random() * validPicks.length)];
     //Rock
     if (userPick == "rock" && botPicked == "rock") return message.channel.send(`You picked ${userPick} and I picked ${botPicked}. It's a tie!`);
     if (userPick == "rock" && botPicked == "paper") return message.channel.send(`You picked ${userPick} and I picked ${botPicked}. I beat you! :sweat_smile:`);
-    if (userPick == "rock" && botPicked == "scrissors") return message.channel.send(`You picked ${userPick} and I picked ${botPicked}.You beat me! :rage:`);
+    if (userPick == "rock" && botPicked == "scissors") return message.channel.send(`You picked ${userPick} and I picked ${botPicked}.You beat me! :rage:`);
     //Paper
     if (userPick == "paper" && botPicked == "paper") return message.channel.send(`You picked ${userPick} and I picked ${botPicked}. It's a tie!`);
     if (userPick == "paper" && botPicked == "rock") return message.channel.send(`You picked ${userPick} and I picked ${botPicked}. You beat me! :rage:`);
-    if (userPick == "paper" && botPicked == "scrissors") return message.channel.send(`You picked ${userPick} and I picked ${botPicked}. I beat you! :sweat_smile:`);
+    if (userPick == "paper" && botPicked == "scissors") return message.channel.send(`You picked ${userPick} and I picked ${botPicked}. I beat you! :sweat_smile:`);
     //Scrissors
-    if (userPick == "scrissors" && botPicked == "scrissors") return message.channel.send(`You picked ${userPick} and I picked ${botPicked}. It's a tie!`);
-    if (userPick == "scrissors" && botPicked == "rock") return message.channel.send(`You picked ${userPick} and I picked ${botPicked}. I beat you! :sweat_smile:`);
-    if (userPick == "scrissors" && botPicked == "paper") return message.channel.send(`You picked ${userPick} and I picked ${botPicked}. You beat me! :rage:`);
+    if (userPick == "scissors" && botPicked == "scissors") return message.channel.send(`You picked ${userPick} and I picked ${botPicked}. It's a tie!`);
+    if (userPick == "scissors" && botPicked == "rock") return message.channel.send(`You picked ${userPick} and I picked ${botPicked}. I beat you! :sweat_smile:`);
+    if (userPick == "scissors" && botPicked == "paper") return message.channel.send(`You picked ${userPick} and I picked ${botPicked}. You beat me! :rage:`);
 };
 
 };

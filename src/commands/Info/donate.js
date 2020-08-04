9c448f504fa6a15662dd158f8b17c9f8895fc92a
Wolfkid200444 <wolfkid200444@gmail.com>
2020-08-04 17:56:52 -0400

    Donate

--- src/commands/Info/donate.js
+++ src/commands/Info/donate.js
@@ -20,7 +20,7 @@ module.exports = class extends Command {
 	  for the time being this is the only thing available for us the reason for this pool is to improve the bot development and hopefully move the bot a suitable 
 	  place were he will be able use more extra features, like ranks, custom prefix the options are infinite but in our current state we cannot afford the hosting 
 	  expensives or even a database.`)
-	  .addBlankField()
+	  .addField(`\u200b`, `\u200b`)
 	  .addField(`\u200b`, `[DONATE HERE](https://paypal.me/pools/c/8rhYAdLdZP) Remember must have a paypal this will change in the future`)
 	  messge.channel.send(embed);
     }

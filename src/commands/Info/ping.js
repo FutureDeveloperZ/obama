eec12c1f63d4a422a4d087d1872e4c92a50641bd
Wolfkid200444 <wolfkid200444@gmail.com>
2020-08-03 20:10:36 -0400

    Added ping

---
+++ src/commands/Info/ping.js
@@ -0,0 +1,29 @@
+const Discord = require("discord.js");
+const Command = require('../../Structure/Command');
+
+module.exports = class extends Command {
+
+	constructor(...args) {
+		super(...args, {
+			aliases: [],
+			description: 'Check the Latency of the bot',
+			category: 'Utilities',
+			usage: ''
+		});
+	}
+
+	async run(message, args) {
+	let author = message.author;
+
+	const embed = new Discord.MessageEmbed()
+		.setAuthor(author.username)
+		.addField("📊 API Latency:","`" + `${Math.round(this.client.ws.ping)}` + "`ms")
+        .setColor('#0017FF');
+        
+	message.channel.send({ embed });
+};
+
+
+exports.help = {
+	name: "ping"
+};

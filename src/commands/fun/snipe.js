7dab768a55d45bb80d13200acbd7a7e75b57e209
Wolfkid200444 <wolfkid200444@gmail.com>
2020-08-03 22:22:46 -0400

    Snipe Fixed

--- src/commands/fun/snipe.js
+++ src/commands/fun/snipe.js
@@ -15,7 +15,7 @@ module.exports = class extends Command {
 	
 async run(message, args) {
     
-    const msg = client.snipes.get(message.channel.id)
+    const msg = this.client.snipes.get(message.channel.id)
     if(!msg) return message.channel.send("There are no deleted messages in this channel!")
     const embed = new Discord.MessageEmbed()
     .setAuthor(msg.author)

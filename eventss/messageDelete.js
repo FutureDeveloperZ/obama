0390efc4607a4c6239313c79c180b90cf3bc520d
Wolfie <Wolfkid200444@gmail.com>
2021-02-14 10:22:27 -0400

    Delete eventss directory

--- eventss/messageDelete.js
+++
@@ -1,9 +0,0 @@
-module.exports = async(message, client) => {
-client.snipes = new Map()
-  
-  client.snipes.set(message.channel.id, {
-    content:message.content,
-    author:message.author.tag,
-    image:message.attachments.first() ? message.attachments.first().proxyURL : null
-  })
-}
